e4717b6bfc47c7cd35b16cf486b0ba775541ffef
FaresMostafa001 <faresmostafa625@gmail.com>
2024-03-25 04:37:28 +0200

    sign up some alerts

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
-import { Button, Label, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Alert, Button, Label, TextInput } from 'flowbite-react'
 import { Link } from 'react-router-dom'
+
 export default function SignIn() {
+  const [formData,setFormData]=useState({});
+  const[errorMessage,setErrorMessage]=useState(null);
+
+  const handleChange =(e)=>{
+    setFormData({...formData,[e.target.id]: e.target.value});
+  }
+  
+  const handleSubmit =async(e)=>{
+    e.preventDefault();
+    if(!formData.username || formData.email ||formData.password){
+      return setErrorMessage('please fill our fields to continue!')
+  
+    }
+    try{
+
+      const res=await fetch('',{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body: JSON.stringify9(formData)
+   
+      })
+    const data = await res.json();
+    if (data.success === false) {
+return setErrorMessage(data.message)    }
+
+  }
+  
+
+    catch(error){
+    }
+  }
+
   return (
     <div className='min-h-screen mt-20'>
       {/* left */}
@@ -27,22 +60,23 @@ export default function SignIn() {
       {/* right */}
       <div className='flex-1'>
         <div>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div className=''>
               <Label value='your user name?' />
               <TextInput 
                 type='text'
                 placeholder='UserName'
-                id='username'
+                id='username'  onChange={handleChange}
               />
             </div>
 
             <div className=''>
               <Label value='your Email?' />
               <TextInput 
-                type='text'
+                type='email'
                 placeholder='Email?'
-                id='email'
+                id='email' onChange={handleChange}
+                 
               />
             </div>
 
@@ -51,7 +85,7 @@ export default function SignIn() {
               <TextInput 
                 type='password'
                 placeholder='Password?'
-                id='password'
+                id='password'  onChange={handleChange}
               />
             </div>
 
@@ -65,6 +99,14 @@ export default function SignIn() {
               Sign In now!
               </Link>
           </div>
+          {
+            errorMessage &&(
+              <Alert className='text-xl mt-5'color='failure' >
+
+              {errorMessage}
+              </Alert>
+  )
+          }
         </div>
 
         </div>
